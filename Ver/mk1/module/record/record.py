492dbb58fc452176d0738acfbb3b4c09887466fd
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2025-03-03 11:01:50 +0900

    update

---
+++ Ver/mk1/module/record/record.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+class Record:
+  def __init__(self):
+      
---
+++ Ver/mk2/date.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import re
+
+
+today = datetime.now()
+
+def split_input(input):
+    return input.split('.')
+
+
+class Date:
+    def __init__(self):
+        cmd = []
+        pass
+
+
+print(split_input('2021.1.4'))
+
+
+
+class Period:
+    def __init__(self):
+        pass
+
+
+
+class DateManager:
+    def __init__(self):
+        pass
